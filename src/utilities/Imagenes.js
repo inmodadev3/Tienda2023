27246b451b84af8796c44cfacfbad0cc06bd906c
sistemas2 <andaoro1@gmail.com>
2024-02-28 15:23:48 -0500

    cambiar imagen en caso de que no se encuentre la ruta principal

--- src/utilities/Imagenes.js
+++ src/utilities/Imagenes.js
@@ -1,6 +1,6 @@
 import banner from '../assets/img/homebg.jpg'
 import logo from '../assets/img/Logo.jpg'
-import sold_out from '../assets/img/sold_out.png'
+import sold_out from '../assets/img/PROXIMAMENTE.jpg'
 
 export const Tienda_Banner =  banner
 export const Inmoda_Logo =  logo
